18c2ae3148fae9cf4ff9fb201ab7f74ee186e9f4
petar.velkov <petar.velkov@uber.com>
2024-02-18 13:11:54 +0200

    Extract main into methods.

--- main.py
+++ main.py
@@ -4,19 +4,30 @@ import argparse
 from src.repl.repl import start, start_with_file
 
 
-def main():
+def greet_user():
+    username = getpass.getuser()
+    print(f"Hello {username}! This is the Monkey programming language!")
+    print("Feel free to type in commands")
+
+
+def get_for_file_input():
     parser = argparse.ArgumentParser(description="Process a file.")
     parser.add_argument("file_path", type=str, nargs='?', help="The path to the file to process", default="none")
 
     args = parser.parse_args()
-    if args.file_path != "none":
-        start_with_file(args.file_path)
-        return
+    return args.file_path
 
-    username = getpass.getuser()
-    print(f"Hello {username}! This is the Monkey programming language!")
-    print("Feel free to type in commands")
-    start()
+
+def main():
+    file_path = get_for_file_input()
+    file_mode = file_path != "none"
+    if file_mode:
+        print(file_path + " output:")
+        start_with_file(file_path)
+        return
+    else:
+        greet_user()
+        start()
 
 
 if __name__ == "__main__":
